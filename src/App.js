34b435207b65e3aac36d5e7a93d766adf6db0e04
Joshua <jbean96@cs.washington.edu>
2019-05-17 13:17:59 -0700

    Added a error logging mechanism to the main application

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import MaterialTable from 'material-table';
 import mutation_operators from './fields.js'
 
+function ErrorMessage(props) {
+  return <div>{ props.message } <button onClick={ props.clearError }>Close</button></div>
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mutants : []
+      mutants : [],
+      currentMutant : null,
+      error : null
     };
 
     this.handleUpload = this.handleUpload.bind(this);
@@ -18,18 +23,41 @@ class App extends React.Component {
     this.fileReader.onloadend = this.handleFileRead;
   }
 
-  handleFileRead(e) {
-    const content = JSON.parse(this.fileReader.result);
-    //const content = this.fileReader.result;
+  logError(err) {
+    this.setState({ error : err });
+  }
 
-    this.setState({ mutants : content });
+  clearError() {
+    this.setState({ error : null });
+  }
+
+  handleFileRead(e) {
+    var content;
+    try {
+      content = JSON.parse(this.fileReader.result);
+    } catch(err) {
+      this.logError(err);
+      return;
+    }
+
+    // TODO: Check the object structure and make sure it's an array of mutants?
+
+    if (!Array.isArray(content)) {
+      this.logError("Top level json object must be an array");
+    } else if (content.length == 0) {
+      this.logError("Uploaded array is empty");
+    } else {
+      this.setState({ mutants : content });
+    }
   }
 
   handleUpload(ev) {
     ev.preventDefault();
-    // TODO: Display error message if no file uploaded?
-    if (this.fileinput.files.length > 0) {
-      this.fileReader.readAsText(this.fileinput.files[0]);
+
+    if (this.fileInput.files.length > 0) {
+      this.fileReader.readAsText(this.fileInput.files[0]);
+    } else {
+      this.logError("No files selected");
     }
   }
 
@@ -63,16 +91,24 @@ class App extends React.Component {
     )
   }
 
+  createErrorMessage() {
+    if (this.state.error) {
+      return <ErrorMessage message={ this.state.error.toString() }
+                           clearError={ this.clearError.bind(this) } />
+    }
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Mutation Testing Visualization Tool</h1>
         <form onSubmit={ this.handleUpload }>
-          <input ref={(ref) => { this.fileinput = ref; }} type="file" />
+          <input ref={ (ref) => { this.fileInput = ref; } } type="file" />
           <button>Upload</button>
         </form>
         <br></br>
-        <div>{this.createMutantTable()}</div>
+        <div>{ this.createMutantTable() }</div>
+        { this.createErrorMessage() }
       </div>
     );
   }
