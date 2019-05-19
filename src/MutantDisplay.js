974fa4b279af0361d6e63e1013ae5ce2c23ae8ec
Joshua <jbean96@cs.washington.edu>
2019-05-18 17:20:22 -0700

    Adding line highlighting

--- src/MutantDisplay.js
+++ src/MutantDisplay.js
@@ -43,17 +43,22 @@ function MutantTable(props) {
 
 /* Functional component that displays details about an individual mutant */
 class MutantCode extends React.Component {
-    makeCodePanel(code) {
-        console.log(code);
+    makeCodePanel(code, lines) {
         return (
             <SyntaxHighlighter showLineNumbers
                 language='python'
                 style={docco}
-                wrapLines='true'>{code}</SyntaxHighlighter>
+                wrapLines='true'
+                lineProps={(lineNum) => {
+                    if (lines.includes(lineNum)) {
+                        return { class : "mutation" };
+                    }
+                }}>{code}</SyntaxHighlighter>
         );
     }
 
     render() {
+        // TODO: Extract mutated lines for mutant and original
         return (
             <div style={{ maxWidth: '100%' }}>
                 <script>hljs.initHighlightingOnLoad();</script>
@@ -62,11 +67,11 @@ class MutantCode extends React.Component {
                 <div id="container">
                     <div class="panel" id="panel1">
                         <h3>Mutant Code</h3>
-                        {this.makeCodePanel(this.props.mutant.unmutated_output)}
+                        {this.makeCodePanel(this.props.mutant.unmutated_output, [1])}
                     </div>
                     <div class="panel" id="panel2">
                         <h3>Original Code</h3>
-                        {this.makeCodePanel(this.props.mutant.unmutated_output)}
+                        {this.makeCodePanel(this.props.mutant.unmutated_output, [2])}
                     </div>
                     <div id="clear"></div>
                 </div>
