ad193ea0dd1c4f41a7467272d662a668498b860b
Joshua <jbean96@cs.washington.edu>
2019-05-20 23:12:59 -0700

    Added correctly highlighted line numbers and line numbering

--- src/MutantCode.js
+++ src/MutantCode.js
@@ -6,12 +6,13 @@ import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 /* Functional component that displays details about an individual mutant */
 class MutantCode extends React.Component {
-    makeCodePanel(code, lines) {
+    makeCodePanel(code, lines, startLineNo) {
         return (
             <SyntaxHighlighter showLineNumbers
                 language='python'
                 style={docco}
                 wrapLines='true'
+                startingLineNumber={startLineNo}
                 lineProps={(lineNum) => {
                     if (lines.includes(lineNum)) {
                         return { class : "mutation" };
@@ -21,18 +22,22 @@ class MutantCode extends React.Component {
     }
 
     render() {
-        // TODO: Extract mutated lines for mutant and original
+        const mutantLines = [this.props.mutant.mutated_lineno];
         return (
             <div style={{ maxWidth: '100%' }}>
                 <script>hljs.initHighlightingOnLoad();</script>
                 <div id="container">
                     <div class="panel" id="panel1">
                         <h3>Original Code</h3>
-                        {this.makeCodePanel(this.props.mutant.unmutated_output, [1])}
+                        {this.makeCodePanel(this.props.mutant.unmutated_output, 
+                            mutantLines.map(x => x - this.props.mutant.mutated_output_lineno + 1), 
+                            this.props.mutant.mutated_output_lineno)}
                     </div>
                     <div class="panel" id="panel2">
                         <h3>Mutant Code</h3>
-                        {this.makeCodePanel(this.props.mutant.mutated_output, [1])}
+                        {this.makeCodePanel(this.props.mutant.mutated_output, 
+                            mutantLines.map(x => x - this.props.mutant.unmutated_output_lineno + 1), 
+                            this.props.mutant.unmutated_output_lineno)}
                     </div>
                     <div id="clear"></div>
                 </div>
